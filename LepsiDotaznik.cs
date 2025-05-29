0eb78fc006641921f8d7e01b70c7ebfd9f660c58
Padin358 <77782848+Padin358@users.noreply.github.com>
2025-05-29 12:58:29 +0200

    Update LepsiDotaznik.cs

--- LepsiDotaznik.cs
+++ LepsiDotaznik.cs
@@ -12,13 +12,16 @@ var hobby = Console.ReadLine();
 
 Console.WriteLine("\nProgram nyní čeká na stisknutí klávesy ENTER a následně bude pokračovat. (program čeká)");
 Console.ReadLine();
+Console.ForegroundColor = ConsoleColor.Green;
 // Sinus
 var sinus = Math.Sin(Int32.Parse(age)) * pi / 180;
 Console.WriteLine($"Sinus z vašeho věku je: {sinus}");
 // Počet písmen ve jméně
 Console.WriteLine($"Délka vašeho jména je: {name.Length}");
+Console.ForegroundColor = ConsoleColor.Red;
 // Čekání na Enter #2
 Console.WriteLine("\nProgram nyní čeká na stisknutí klávesy ENTER a následně bude pokračovat. (program čeká)");
 Console.ReadLine();
-
+Console.ForegroundColor = ConsoleColor.Green;
 Console.WriteLine($"Výpis ze zadaných informací:\nVaše jméno: {name}\nVáš věk je: {age}\nChodíte do školy: {school}\nVaše záliby jsou: {hobby}\n\nDěkujeme za vyplnění dotazníku!");
+Console.ForegroundColor = ConsoleColor.White;
